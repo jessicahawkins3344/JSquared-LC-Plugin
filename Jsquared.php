27f01d3dc671bae94ece957a215bbb5c60a9a09f
Jessica Hawkins <jessicarhawkins08@outlook.com>
2016-11-13 21:50:57 -0600

    updated plugin info

--- Jsquared.php
+++ Jsquared.php
@@ -1,11 +1,10 @@
 <?php
 /*
-Plugin Name: JSquared
-Plugin URI:  https://developer.wordpress.org/plugins/the-basics/
-Description: Basic WordPress Plugin Header Comment
-Version:     20160911
-Author:      WordPress.org
-Author URI:  https://developer.wordpress.org/
+Plugin Name: JSquared LC Templates
+Description: LiveComposer Template Extensions
+Version:     1.0
+Author:      Jessica Hawkins
+Author URI:  http://www.jsquaredcreative.com
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: wporg
